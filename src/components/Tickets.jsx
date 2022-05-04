2b7006a837828154e356631d468f2f09e4fbefe6
aldangold <meloshnikoff@gmail.com>
2022-05-05 02:20:01 +0300

    added LoadingPage

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector, batch } from 'react-redux';
 import axios from 'axios';
 import ticketsInFile from '../tickets.json';
 import { useTranslation } from 'react-i18next';
+import LoadingPage from './LoadingPage';
 import { actions as ticketsActions } from '../slices/ticketsSlice.js';
 
 const Tickets = () => {
@@ -11,13 +12,11 @@ const Tickets = () => {
 
   const { ticketsStack, reconciledTickets } = useSelector((state) => state.ticketsReducer);
 
-
   useEffect(() => {
     const makeRequest = async () => {
       try {
         const getId = await axios.get('https://front-test.beta.aviasales.ru/search');
-        const searchIdData = await getId.json();
-        const { searchId } = searchIdData;
+        const { searchId } = await getId.json();
 
         const getTickets = await axios.get(`https://front-test.beta.aviasales.ru/tickets?searchId=${searchId}`);
         const { tickets } = await getTickets.json();
@@ -63,37 +62,43 @@ const Tickets = () => {
     const formatM = m > 0 ? `${m}м` : ''; 
     return `${formatH} ${formatM}`.trim();
   }
-  
+
+  if ( !reconciledTickets.length ) {
+    return <LoadingPage/>
+  }
+
   return (
-    <div className='tickets_list flex-column'>
-    {reconciledTickets
-        .slice(0, ticketsStack)
-        .map((ticket, index) => <div className='ticket col-md-9' key={index}>
-        <div className='flex-item flex-grow-1 price'>{(ticket.price).toLocaleString('ru')} Р</div>
-        <div className='flex-item  flex-grow-1 carrier'><img src={`//pics.avs.io/99/36/${ticket.carrier}.png`}></img></div>
-        <div className='destination'>
-            {ticket.segments.map((segment, index) => <div className='route' key={index}>
-            <div className='gr d-flex'>{segment.origin}-{segment.destination}</div>
-            <div className='bk d-flex'>{getTime(segment.date)} - {getTime(getTimeArrival(segment.date, segment.duration))}</div>
-                </div>)}
-        </div>
-        <div className='duration'>
-            {ticket.segments.map((segment, index) => <div className='route' key={index}>
-            <div className='gr d-flex'>{t('ticket.duration')}</div>
-            <div className='bk d-flex'>{formatTime(segment.duration)}</div>
-            </div>)}
-        </div>
-        <div className='stops'>
-            {ticket.segments.map((segment, index) => <div className='route' key={index}>
-            <div className='gr d-flex'>{t('ticket.transfersCount', { count: segment.stops.length })}</div>
-            <div className='bk d-flex'>{segment.stops.map((stop, index) => <span key={index}> {stop} </span>)}</div>
-            </div>)}
-        </div>
-        </div>)}
-        <div className='show-more-tickets'>
-        <button className='show-more-tickets_button' onClick={handler}>{t('showMoreTicketsButton')}</button>
-        </div>
-    </div>
+    <>
+      <div className='tickets_list flex-column'>
+      {reconciledTickets
+          .slice(0, ticketsStack)
+          .map((ticket, index) => <div className='ticket col-md-9' key={index}>
+          <div className='flex-item flex-grow-1 price'>{(ticket.price).toLocaleString('ru')} Р</div>
+          <div className='flex-item  flex-grow-1 carrier'><img src={`//pics.avs.io/99/36/${ticket.carrier}.png`}></img></div>
+          <div className='destination'>
+              {ticket.segments.map((segment, index) => <div className='route' key={index}>
+              <div className='gr d-flex'>{segment.origin}-{segment.destination}</div>
+              <div className='bk d-flex'>{getTime(segment.date)} - {getTime(getTimeArrival(segment.date, segment.duration))}</div>
+                  </div>)}
+          </div>
+          <div className='duration'>
+              {ticket.segments.map((segment, index) => <div className='route' key={index}>
+              <div className='gr d-flex'>{t('ticket.duration')}</div>
+              <div className='bk d-flex'>{formatTime(segment.duration)}</div>
+              </div>)}
+          </div>
+          <div className='stops'>
+              {ticket.segments.map((segment, index) => <div className='route' key={index}>
+              <div className='gr d-flex'>{t('ticket.transfersCount', { count: segment.stops.length })}</div>
+              <div className='bk d-flex'>{segment.stops.map((stop, index) => <span key={index}> {stop} </span>)}</div>
+              </div>)}
+          </div>
+          </div>)}
+          <div className='show-more-tickets'>
+          <button className='show-more-tickets_button' onClick={handler}>{t('showMoreTicketsButton')}</button>
+          </div>
+      </div>
+    </>
   );
 };
 
